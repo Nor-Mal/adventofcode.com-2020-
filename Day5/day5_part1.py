5c740a812f356d09df73347ea18d5f6768ab6c89
Norbert M <79285346+Nor-Mal@users.noreply.github.com>
2021-06-22 08:52:27 +0100

    Update day5_part1.py

--- Day5/day5_part1.py
+++ Day5/day5_part1.py
@@ -10,6 +10,7 @@ R - right side, keep upper half
 0 - 7 (8 columns of seats)
 
 '''
+# - PART 1 -
 import re
 
 with open('./data.txt', 'r',) as fp:
@@ -25,25 +26,25 @@ for code in data:
     for char in code:
         if char == 'F':
             row_start, row_end = row_start, ((row_end - row_start)//2 + row_start)
-            # print("Letter was {} and range was {} to {}".format(char, row_start, row_end))
             
         elif char == 'B':
             row_start, row_end = row_start + ((row_end - row_start+1)//2), row_end
-            # print("Letter was {} and range was {} to {}".format(char, row_start, row_end))
             
         elif char == 'R':
             column_start, column_end = column_start + ((column_end - column_start+1)//2), column_end
-            # print("Letter was {} and range was {} to {}".format(char, column_start, column_end))
             
         elif char == 'L':
             column_start, column_end = column_start, ((column_end - column_start)//2 + column_start)
-            # print("Letter was {} and range was {} to {}".format(char, column_start, column_end))
-            
-    # print("Final row = {}".format(row_end))
-    # print("Final column = {}".format(column_end))
 
     seat_id = (row_end*8)+column_end
-    # print("Seat ID: {}".format(seat_id))
+ 
     seat_id_list.append(seat_id)
     
 print("Highest seat ID = {}".format(max(seat_id_list)))
+
+# - PART 2 -
+for item in range(len(seat_id_list)-1):
+    if seat_id_list[item+1] - seat_id_list[item] != 1:
+        print("Your seat ID: {}".format(seat_id_list[item]+1))
+
+
