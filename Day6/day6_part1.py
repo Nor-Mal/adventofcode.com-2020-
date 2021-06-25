cccf8d9ab676ff2f909f08ff35be3cd9c43d452f
Norbert M <79285346+Nor-Mal@users.noreply.github.com>
2021-06-25 11:33:25 +0100

    Update day6_part1.py

--- Day6/day6_part1.py
+++ Day6/day6_part1.py
@@ -1,1 +1,10 @@
-
+with open('./data.txt', 'r', ) as fp:
+    entry = []
+    sum_yes = 0
+    for answer in fp.read().split("\n"):
+        if answer != '':
+            [entry.append(x) for x in answer if x not in entry]
+        else:
+            sum_yes += len(entry)
+            entry = []
+print(sum_yes)
